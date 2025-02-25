cc8523b61bcf8baedeb5d30973dfa583949c3309
marlonlambda <marlon.jaramillo@lambdaanalytics.co>
2025-02-25 16:04:36 -0500

    Revert "feat(card) agrega la carta donde se van a visualizar los productos"

--- src/modules/product/ProductPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ProductPage = () => {
-  return (
-    <div className='flex flex-col justify-center gap-y-4'>
-      <Card products={productos}/>
-    </div>
-  )
-}
